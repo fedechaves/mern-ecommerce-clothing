803b52830c146710aad7b10c2b16803940b62869
fedechaves <federicochavesf@gmail.com>
2023-06-02 14:39:56 -0400

    added cors policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,6 +3,14 @@ module.exports = [
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'https://mern-ecommerce-clothing.vercel.app']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
